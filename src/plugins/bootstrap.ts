123835a98526adb26f8f0d6eccae220ea4880e11
Alexandre <alexgaspar@hotmail.fr>
2018-05-21 16:44:55 +0200

    clear

--- src/plugins/bootstrap.ts
+++ src/plugins/bootstrap.ts
@@ -5,17 +5,15 @@ import en from 'vee-validate/dist/locale/en';
 
 import clickout from './directives/clickoutside';
 
-/*
-  Register directives
-*/
-Vue.directive('clickout', clickout);
-
-/*
-  Register plugins
-*/
 export default ({ store, app }, inject) => {
-  // Mettre l'instance `i18n` dans `app`
-  // De cette manière nous pouvons l'utiliser comme middleware et `asyncData` / `fetch` pour les pages
+  /*
+    Register directives
+  */
+  Vue.directive('clickout', clickout);
+
+  /*
+    Register plugins
+  */
   Vue.use(VeeValidate, {
     i18n: app.i18n,
     dictionary: {
